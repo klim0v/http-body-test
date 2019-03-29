e3b7deb7f4e3c132f1263d013022fdf2a080edd4
klimov sergey <klim0v-sergey@yandex.ru>
2019-03-29 19:10:14 +0300

    wip

--- service/service.go
+++ service/service.go
@@ -1,19 +1,35 @@
 package service
 
 import (
+	"bytes"
+	"encoding/csv"
 	"github.com/golang/protobuf/ptypes/empty"
 	"golang.org/x/net/context"
 	"google.golang.org/genproto/googleapis/api/httpbody"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Service struct {
 }
 
 func (*Service) GetResource(ctx context.Context, in *empty.Empty, opts ...grpc.CallOption) (*httpbody.HttpBody, error) {
+	var buf bytes.Buffer
+	writer := csv.NewWriter(&buf)
+	writer.Comma = ';'
+
+	err := writer.Write([]string{"1", "gopher conference", "$100"})
+	if err != nil {
+		return new(httpbody.HttpBody), status.New(codes.Internal, err.Error()).Err()
+	}
+
+	writer.Flush()
+
 	response := &httpbody.HttpBody{
-		ContentType: "text/html",
-		Data:        []byte("Hello World"),
+		//Extensions: ,
+		ContentType: "application/octet-stream",
+		Data:        buf.Bytes(),
 	}
 	return response, nil
 }
